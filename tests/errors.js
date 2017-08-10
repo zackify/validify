fb3d37e12d5550cd81503f948c8499df860dac88
Ryan Castner <castner.rr@gmail.com>
2017-08-10 14:37:19 -0400

    Add tests for custom error messages

--- tests/errors.js
+++ tests/errors.js
@@ -17,3 +17,52 @@ test('Form sets errors from prop', () => {
 
   expect(wrapper.find(Input).props().error).toEqual('failed because blah');
 });
+
+
+test('Form sets custom error message from prop', () => {
+  const wrapper = shallow(
+    <Form rules={{test: 'required|min:8'}} errorMessages={{'min.test': 'I am a custom message!', 'min.required': 'wow, so field, much required.' }}>
+      <Input name="test" />
+      <button submit className="button"></button>
+    </Form>
+  );
+
+  wrapper.find(Input).simulate('change', { target: { name: 'test', value: 'fail' }});
+  wrapper.find(Input).simulate('blur');
+  expect(wrapper.find(Input).props().error).toEqual('I am a custom message!');
+});
+
+test('Form sets custom error message on submit', () => {
+  const wrapper = shallow(
+    <Form initialValues={{test: '' }} rules={{test: 'required'}} errorMessages={{ 'min.test': 'I am a custom message!', 'required.test': 'wow, so field, much required.' }}>
+      <Input name="test" />
+      <button submit className="button"></button>
+    </Form>
+  );
+  wrapper.find('.button').simulate('click');
+  expect(wrapper.find(Input).props().error).toEqual('wow, so field, much required.');
+});
+
+test('Form sets custom error message when an errorMessage key is not used', () => {
+  const wrapper = shallow(
+    <Form initialValues={{test: '' }} rules={{test: 'required'}} errorMessages={{'required.other': 'Heyo', 'min.test': 'I am a custom message!', 'required.test': 'wow, so field, much required.' }}>
+      <Input name="test" />
+      <button submit className="button"></button>
+    </Form>
+  );
+  wrapper.find('.button').simulate('click');
+  expect(wrapper.find(Input).props().error).toEqual('wow, so field, much required.');
+});
+
+test('Form sets custom error message when multiple errorMessage keys are used', () => {
+  const wrapper = shallow(
+    <Form initialValues={{test: '', other: 'roar'}} rules={{test: 'required', other: 'max:3'}} errorMessages={{'max.other': 'Heyo', 'min.test': 'I am a custom message!', 'required.test': 'wow, so field, much required.' }}>
+      <Input name="test" />
+      <Input name="other" />
+      <button submit className="button"></button>
+    </Form>
+  );
+  wrapper.find('.button').simulate('click');
+  expect(wrapper.find(Input).first().props().error).toEqual('wow, so field, much required.');
+  expect(wrapper.find(Input).last().props().error).toEqual('Heyo');
+});
