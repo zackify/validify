24f4cc421131d6a72f4015f237ed58eff5fd82cd
Zach Silveira <zackify@gmail.com>
2017-09-15 09:19:56 -0400

    Better value checking

--- src/input.js
+++ src/input.js
@@ -2,8 +2,23 @@
 import React from 'react';
 import getError from './get-error';
 
+const filters = {
+  checkbox: value => value || value === false,
+  radio: value => value || value === false,
+  default: value => value,
+};
+
+const getValue = (type, values) => {
+  let filter = filters[type] || filters.default;
+
+  let value = values.filter(filter)[0];
+  if (filter(value)) return value;
+
+  return '';
+};
+
 export default (child, propValues, children, errors, component) => {
-  let { name, onKeyUp, onEnter } = child.props;
+  let { name, onKeyUp, onEnter, type } = child.props;
 
   return React.cloneElement(child, {
     children,
@@ -11,12 +26,7 @@ export default (child, propValues, children, errors, component) => {
       component.validateOnBlurOrChange(name, () => component.onChange(e)),
     onBlur: () => component.validateOnBlurOrChange(name),
     error: getError(errors, component.props.errors, name),
-    value:
-      component.state.values[name] || component.state.values[name] === false
-        ? component.state.values[name]
-        : propValues[name] || propValues[name] === false
-          ? propValues[name]
-          : '',
+    value: getValue(type, [component.state.values[name], propValues[name]]),
     onKeyUp: e => {
       if (onKeyUp) onKeyUp(e);
       if (e.keyCode !== 13) return;
